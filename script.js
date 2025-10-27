172e196e7d208fb7ac3443d0f205c51729f5eb37
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-10-27 17:47:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("voteForm").addEventListener("submit", function (event) 
 
   // Validation
   if (name === "" || age === "") {
-    alert("Please enter valid details.");
+    alert("Please enter valid details");
     return;
   }
 
